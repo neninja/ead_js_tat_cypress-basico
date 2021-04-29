e1b5a9cafe29660996ca2b5525eb1d9820d33609
Felipe Silva <felipe.dacs@gmail.com>
2021-04-29 00:25:08 -0300

    :tv: add aula 3

--- cypress/integration/tickets.spec.js
+++ cypress/integration/tickets.spec.js
@@ -1,6 +1,21 @@
 describe('Tickets', () => {
   // beforeEach(() => cy.visit('https://ticket-box.s3.eu-central-1.amazonaws.com/index.html'))
   beforeEach(() => cy.visit('http://localhost:2222'))
+
+  it('fills all the input fields', () => {
+    const firstName = 'Walmyr'
+    const lastName = 'Filho'
+    cy.get('#first-name').type(firstName)
+    cy.get('#last-name').type(lastName)
+    cy.get('#email').type('abc@gmail.com')
+    cy.get('#requests').type('Vegetarian')
+    cy.get('#signature').type(`${firstName} ${lastName}`)
+  })
+
+  it.only('select two tickets', () => {
+    cy.get('#ticket-quantity').select('2')
+  })
+
   it(`has "TICKETBOX" header's heading`, () => {
   })
 })
